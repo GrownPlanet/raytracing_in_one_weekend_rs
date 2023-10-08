58aaa0c8d49ed0b9164b9b9617a2b2f8d6b24aac
Timo Dehandschutter <timo.dehandschutter@gmail.com>
2023-10-08 10:32:33 +0200

    open file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::{
+    fs::{self, File},
+    io::prelude::*,
+    path::Path,
+};
+
 fn main() {
+    // open file to put image in
+    let path = "image.ppm";
+    if Path::new(path).exists() {
+        fs::remove_file(path).unwrap();
+    }
+
+    let mut file = File::create(path);
+
     println!("Hello, world!");
 }

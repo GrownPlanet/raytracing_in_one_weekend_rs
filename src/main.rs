7ba6bd59687ac52b66e554b72b55fdf608c8cdd7
Timo Dehandschutter <timo.dehandschutter@gmail.com>
2023-10-09 10:57:00 +0200

    small formating change

--- src/main.rs
+++ src/main.rs
@@ -96,6 +96,7 @@ fn ray_color(ray: &Ray) -> Color {
     let t = hit_sphere(&Point3::new(0., 0., -1.), 0.5, ray);
     if t > 0. {
         let n = (ray.clone().at(t) - Point3::new(0., 0., -1.)).unit_vector();
+        // map `-1` to `1` to `0` to `1`
         return Color::new(n.x + 1., n.y + 1., n.z + 1.) * 0.5;
     }
 

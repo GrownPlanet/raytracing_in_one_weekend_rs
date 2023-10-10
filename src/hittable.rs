0aea73724d4f6fe13a9245eb5fa8e52179b6e28b
Timo Dehandschutter <timo.dehandschutter@gmail.com>
2023-10-10 14:26:34 +0200

    i dont know anymore

--- src/hittable.rs
+++ src/hittable.rs
@@ -4,6 +4,19 @@ pub struct HitRecord {
     pub point: Point3,
     pub normal: Point3,
     pub t: f64,
+    pub front_face: bool,
+}
+
+impl HitRecord {
+    pub fn set_face_normal(&mut self, ray: &Ray, outward_normal: &Point3) {
+        self.front_face = Point3::dot(&ray.dir, outward_normal) < 0.;
+
+        self.normal = if self.front_face {
+            outward_normal.clone()
+        } else {
+            outward_normal.clone() * -1.
+        }
+    }
 }
 
 pub trait Hittable {
